f9d4283738066ef34130bd80b2f589b25ea6319f
kwood15 <kwood15@users.noreply.github.com>
2020-02-04 16:06:06 +0000

    Add additional templates for components

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,78 @@
 module.exports = function(plop) {
   plop.setGenerator("React Function Component", {
-    description: "A stateless function component",
+    description: "A Function Component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component name"
+      },
+      {
+        type: "directory",
+        name: "path",
+        message: "Component folder",
+        basePath: "./src/components"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "./src/components/{{path}}/{{pascalCase name}}.tsx",
+        templateFile: "plop-templates/component-function.hbs",
+        skipIfExists: true
+      }
+    ]
+  });
+  plop.setGenerator("React Function Component with Hooks", {
+    description: "A Function Component with Hooks",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component name"
+      },
+      {
+        type: "directory",
+        name: "path",
+        message: "Component folder",
+        basePath: "./src/components"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "./src/components/{{path}}/{{pascalCase name}}.tsx",
+        templateFile: "plop-templates/component-function-with-hooks.hbs",
+        skipIfExists: true
+      }
+    ]
+  });
+  plop.setGenerator("React Arrow Function", {
+    description: "A React Arrow Function",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component name"
+      },
+      {
+        type: "directory",
+        name: "path",
+        message: "Component folder",
+        basePath: "./src/components"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "./src/components/{{path}}/{{pascalCase name}}.tsx",
+        templateFile: "plop-templates/component-function-arrow.hbs",
+        skipIfExists: true
+      }
+    ]
+  });
+  plop.setGenerator("React Class Component", {
+    description: "A React Class component",
     prompts: [
       {
         type: "input",
@@ -17,8 +89,8 @@ module.exports = function(plop) {
     actions: [
       {
         type: "add",
-        path: "./src/{{path}}/{{pascalCase name}}.tsx",
-        templateFile: "plop-templates/component.hbs",
+        path: "./src/components/{{path}}/{{pascalCase name}}.tsx",
+        templateFile: "plop-templates/component-class.hbs",
         skipIfExists: true
       }
     ]
